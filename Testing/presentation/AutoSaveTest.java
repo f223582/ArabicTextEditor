ac208fe7102d90b8c4f2495eca2393c780d766bf
Ayesha Mudassar <f228761@cfd.nu.edu.pk>
2026-02-15 17:48:18 +0500

    Added JUnit Testing for AutoSaveTest.java

--- Testing/presentation/AutoSaveTest.java
+++ Testing/presentation/AutoSaveTest.java
@@ -1,5 +1,31 @@
-package presentation;
+package Testing.presentation;
+
+import presentation.TextEditor;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AutoSaveTest {
-    
+
+    @Test
+    void testAutoSaveTriggered() {
+        TextEditor editor = new TextEditor();
+        String content = "word ".repeat(501);
+
+        assertTrue(editor.shouldAutoSave(content));
+    }
+
+    @Test
+    void testAutoSaveNotTriggered() {
+        TextEditor editor = new TextEditor();
+        String content = "word ".repeat(500);
+
+        assertFalse(editor.shouldAutoSave(content));
+    }
+
+    @Test
+    void testAutoSaveEmpty() {
+        TextEditor editor = new TextEditor();
+
+        assertFalse(editor.shouldAutoSave(""));
+    }
 }
