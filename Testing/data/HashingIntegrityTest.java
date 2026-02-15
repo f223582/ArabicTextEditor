b84d6188d25f9f1606db99115b389b04d175b998
Ayesha Mudassar <f228761@cfd.nu.edu.pk>
2026-02-15 17:51:11 +0500

    Added JUnit Testing for HashingIntegrityTest.java

--- Testing/data/HashingIntegrityTest.java
+++ Testing/data/HashingIntegrityTest.java
@@ -1,5 +1,24 @@
-package data;
+package Testing.data;
+
+import data.persistence.HashUtil;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class HashingIntegrityTest {
-    
+
+    @Test
+    void testHashChangesAfterEdit() {
+        String hash1 = HashUtil.generateMD5("Original");
+        String hash2 = HashUtil.generateMD5("Edited");
+
+        assertNotEquals(hash1, hash2);
+    }
+
+    @Test
+    void testSameContentSameHash() {
+        String hash1 = HashUtil.generateSHA1("Same");
+        String hash2 = HashUtil.generateSHA1("Same");
+
+        assertEquals(hash1, hash2);
+    }
 }
