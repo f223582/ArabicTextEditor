945d388de62dbf20230eff05268b3af2b0fdc6a9
Ayesha Mudassar <f228761@cfd.nu.edu.pk>
2026-02-15 17:49:46 +0500

    Added JUnit Testing for ExportCommandTest.java

--- Testing/business/ExportCommandTest.java
+++ Testing/business/ExportCommandTest.java
@@ -1,5 +1,20 @@
-package business;
+package Testing.business;
+
+import business.commands.ExportCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ExportCommandTest {
-    
+
+    @Test
+    void testValidExport() {
+        ExportCommand cmd = new ExportCommand("file.txt", "content");
+        assertDoesNotThrow(cmd::execute);
+    }
+
+    @Test
+    void testNullContentExport() {
+        ExportCommand cmd = new ExportCommand("file.txt", null);
+        assertThrows(IllegalArgumentException.class, cmd::execute);
+    }
 }
