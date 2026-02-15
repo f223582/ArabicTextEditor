d1e3078233d2f4961926e129a23c305c3e942397
Ayesha Mudassar <f228761@cfd.nu.edu.pk>
2026-02-15 17:49:09 +0500

    Added JUnit Testing for ImportCommandTest.java

--- Testing/business/ImportCommandTest.java
+++ Testing/business/ImportCommandTest.java
@@ -1,5 +1,20 @@
-package business;
+package Testing.business;
+
+import business.commands.ImportCommand;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ImportCommandTest {
-    
+
+    @Test
+    void testValidImport() {
+        ImportCommand cmd = new ImportCommand("sample.txt");
+        assertDoesNotThrow(cmd::execute);
+    }
+
+    @Test
+    void testInvalidImport() {
+        ImportCommand cmd = new ImportCommand("");
+        assertThrows(IllegalArgumentException.class, cmd::execute);
+    }
 }
