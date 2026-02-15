972e96a233caab9b91cb897952e3a4c4b40cee40
Ayesha Mudassar <f228761@cfd.nu.edu.pk>
2026-02-15 17:50:44 +0500

    Added JUnit Testing for TFIDFServiceTest.java
    Updated package declaration and added test cases for TFIDFService.

--- Testing/business/TFIDFServiceTest.java
+++ Testing/business/TFIDFServiceTest.java
@@ -1,5 +1,24 @@
-package business;
+package Testing.business;
+
+import business.services.TFIDFService;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TFIDFServiceTest {
-    
+
+    @Test
+    void testPositiveTFIDF() {
+        TFIDFService service = new TFIDFService();
+        double score = service.calculateTFIDF("word word test", "word");
+
+        assertEquals(0.66, score, 0.01);
+    }
+
+    @Test
+    void testEmptyDocument() {
+        TFIDFService service = new TFIDFService();
+        double score = service.calculateTFIDF("", "word");
+
+        assertEquals(0.0, score);
+    }
 }
