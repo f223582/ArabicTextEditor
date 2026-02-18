96b1d0c0cb13d75ebe0199466341a909d987d074
f223582 <f223582@cfd.nu.edu.pk>
2026-02-18 22:14:44 +0500

    Fixed DatabaseSingletonTest's errors

--- Testing/data/DatabaseSingletonTest.java
+++ Testing/data/DatabaseSingletonTest.java
@@ -1,16 +1,29 @@
-package Testing.data;
+package data;
 
-import data.persistence.DatabaseConnection;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import dal.DatabaseConnection;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.*;
 
 public class DatabaseSingletonTest {
 
     @Test
-    void testSingletonInstance() {
+    public void testSingletonInstance() {
+
         DatabaseConnection db1 = DatabaseConnection.getInstance();
         DatabaseConnection db2 = DatabaseConnection.getInstance();
 
         assertSame(db1, db2);
+    }
+
+    @Test
+    public void testConnectionNotNull() {
+
+        DatabaseConnection db = DatabaseConnection.getInstance();
+        Connection connection = db.getConnection();
+
+        assertNotNull(connection);
     }
 }
